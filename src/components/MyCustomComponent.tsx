8ec4a883e361e82941b2d356b65cd38389a94a72
aito <BoxPistols@users.noreply.github.com>
2023-06-22 22:04:10 +0900

    init

---
+++ src/components/MyCustomComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Button } from "@mui/material"
+
+const MyCustomComponent: React.VFC = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Click Me
+    </Button>
+  )
+}
+
+export default MyCustomComponent
